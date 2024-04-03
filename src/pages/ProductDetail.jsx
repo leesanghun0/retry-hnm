bdfdcd32487aca26d2d8970f4ed9a451bb730052
leesanghun <lsh93620@gmail.com>
2024-04-04 01:05:35 +0900

    Detail/useSelector//selecteditem->productList로 변경

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -6,7 +6,7 @@ import { fetchProductDetail } from "../redux/reducers/productSlice";
 
 
 const ProductDetail =()=>{
-    const product = useSelector((state) => state.product.selectedItem);
+    const product = useSelector((state) => state.product.ProductList);
     //const [detail, setDetail] = useState();
     let {id} = useParams();
     const dispatch = useDispatch();
